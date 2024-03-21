3aa665f611fbf43aa919047be706fea5004372d0
Max Kalhama <kalhama.max@gmail.com>
2024-03-21 21:20:31 +0200

    fix: hydration error

--- src/components/menu.tsx
+++ src/components/menu.tsx
@@ -12,25 +12,8 @@ import { Separator } from '@/components/ui/separator'
 import { Menu as MenuIcon } from 'lucide-react'
 import Link from 'next/link'
 import * as React from 'react'
-import { useMediaQuery } from 'react-responsive'
 
 export const Menu = () => {
-  return (
-    <div>
-      <div className="flex justify-between items-center p-4">
-        <h1>App logo</h1>
-        <MenuLinks />
-      </div>
-      <Separator orientation="horizontal" />
-    </div>
-  )
-}
-
-const MenuLinks = () => {
-  const isDesktop = useMediaQuery({
-    query: 'only screen and (max-width : 768px)',
-  })
-
   const links = (
     <>
       <Link className="hover:underline" href="/1">
@@ -44,26 +27,30 @@ const MenuLinks = () => {
       </Link>
     </>
   )
-
-  if (isDesktop) {
-    return (
-      <Drawer direction="right">
-        <DrawerTrigger asChild>
-          <Button variant="ghost">
-            <MenuIcon />
-          </Button>
-        </DrawerTrigger>
-        <DrawerContent className="h-screen top-0 right-0 left-auto mt-0 w-[20rem] max-w-full rounded-none">
-          <div className="p-4 pb-0 flex flex-col gap-2">{links}</div>
-          <DrawerFooter>
-            <DrawerClose asChild>
-              <Button variant="outline">Close</Button>
-            </DrawerClose>
-          </DrawerFooter>
-        </DrawerContent>
-      </Drawer>
-    )
-  } else {
-    return <div className="flex gap-4">{links}</div>
-  }
+  return (
+    <div>
+      <div className="flex justify-between items-center p-4">
+        <h1>App logo</h1>
+        <div className="block md:hidden">
+          <Drawer direction="right">
+            <DrawerTrigger asChild>
+              <Button variant="ghost">
+                <MenuIcon />
+              </Button>
+            </DrawerTrigger>
+            <DrawerContent className="h-screen top-0 right-0 left-auto mt-0 w-[20rem] max-w-full rounded-none">
+              <div className="p-4 pb-0 flex flex-col gap-2">{links}</div>
+              <DrawerFooter>
+                <DrawerClose asChild>
+                  <Button variant="outline">Close</Button>
+                </DrawerClose>
+              </DrawerFooter>
+            </DrawerContent>
+          </Drawer>
+        </div>
+        <div className="hidden md:flex gap-4">{links}</div>
+      </div>
+      <Separator orientation="horizontal" />
+    </div>
+  )
 }
